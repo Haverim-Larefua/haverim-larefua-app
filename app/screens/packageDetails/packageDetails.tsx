cfbec85e9dca239ce53f3c748b419b035f9518ef
Menashe, Or (om2461) <om2461@intl.att.com>
2020-02-09 16:19:31 +0200

    rearrange package details code

--- app/screens/packageDetails/packageDetails.tsx
+++ app/screens/packageDetails/packageDetails.tsx
@@ -1,10 +1,10 @@
 import * as React from "react"
-import {SafeAreaView, View} from "react-native"
-import {Button, Screen, Text} from "../../components"
-import {PackageData, PackageStatus} from "../packagesList/types"
-import {PackageStatusHeader} from "./packageStatusHeader"
-import {NavigationInjectedProps} from "react-navigation"
-import {color} from "../../theme"
+import { SafeAreaView, StyleSheet, View } from "react-native"
+import { Button, Screen, Text } from "../../components"
+import { PackageData, PackageStatus } from "../packagesList/types"
+import { PackageStatusHeader } from "./packageStatusHeader"
+import { NavigationInjectedProps } from "react-navigation"
+import { color } from "../../theme"
 
 interface PackageDetailsScreenProps {
     packageData: PackageData
@@ -12,34 +12,62 @@ interface PackageDetailsScreenProps {
 
 export const PackageDetailsScreen: React.FunctionComponent<NavigationInjectedProps<PackageDetailsScreenProps>> = props => {
   const packageData = props.navigation.state.params.packageData
+  const renderFullNameView = (): React.ReactElement => {
+    return (
+      <View style={styles.fullNameView}>
+        <Text preset={'header'} text={`${packageData.receiver.lastName} ${packageData.receiver.firstName}`} />
+      </View>
+    )
+  }
+
+  const renderDetailsView = (title: string, isTitleBolded = false, explanationText: string): React.ReactElement => {
+    return (
+      <View style={styles.detailsView}>
+        <View>
+          <Text preset={isTitleBolded ? 'bold' : 'default'} text={title}/>
+          <Text text={explanationText} />
+        </View>
+      </View>
+    )
+  }
+
+  const renderApproveButton = (): React.ReactElement => {
+    return (
+      <SafeAreaView style={{ flex: 1, justifyContent: 'flex-end' }}>
+        <Button style={{ marginHorizontal: 12, marginBottom: 12 }} text={packageData.status === PackageStatus.ReadyForDelivery ? 'איסוף חבילה' : 'מסירת חבילה'} />
+      </SafeAreaView>
+    )
+  }
+
   return (
     <Screen preset="fixed" >
       <PackageStatusHeader packageData={packageData}/>
-      <View style={{ paddingTop: 22, paddingRight: 20, paddingBottom: 18, borderColor: color.palette.lighterGrey, borderWidth: 1 }}>
-        <Text preset={'header'} text={`${packageData.receiver.lastName} ${packageData.receiver.firstName}`} />
-      </View>
-      <View style={{ flexDirection: 'row-reverse', paddingTop: 22, paddingBottom: 24, paddingHorizontal: 20, borderColor: color.palette.lighterGrey, borderBottomWidth: 1 }}>
-        <View>
-          <Text text={'טלפון'}/>
-          <Text text={'052486589'} />
-        </View>
-      </View>
-      <View style={{ flexDirection: 'row-reverse', paddingTop: 22, paddingBottom: 24, paddingHorizontal: 20, borderColor: color.palette.lighterGrey, borderBottomWidth: 1 }}>
-        <View>
-          <Text text={packageData.destination.city}/>
-          <Text preset={'default'} text={`${packageData.destination.street} ${packageData.destination.number}/${packageData.destination.apartment}`} />
-        </View>
-      </View>
-      <View style={{ paddingTop: 14, paddingHorizontal: 20, flex: 1 }}>
-        <Text text={'פרטים נוספים'} />
-        <Text text={'יש שער גדול והקוד הוא 7364. יש בחצר כלב אז אם הוא ישן נא לא להעיר אותו'}/>
-      </View>
+      {renderFullNameView()}
+      {renderDetailsView('טלפון', false, '0524897564')}
+      {renderDetailsView(packageData.destination.city, false, `${packageData.destination.street} ${packageData.destination.number}/${packageData.destination.apartment}`)}
+      {renderDetailsView('פרטים נוספים', true, 'יש שער גדול והקוד הוא 7364. יש בחצר כלב אז אם הוא ישן נא לא להעיר אותו')}
       {
         packageData.status !== PackageStatus.Delivered &&
-            <SafeAreaView>
-              <Button style={{ marginHorizontal: 12, marginBottom: 12 }} text={packageData.status === PackageStatus.ReadyForDelivery ? 'איסוף חבילה' : 'מסירת חבילה'} />
-            </SafeAreaView>
+        renderApproveButton()
       }
     </Screen>
   )
 }
+
+const styles = StyleSheet.create({
+  detailsView: {
+    borderBottomWidth: 1,
+    borderColor: color.palette.lighterGrey,
+    flexDirection: 'row-reverse',
+    paddingBottom: 24,
+    paddingHorizontal: 20,
+    paddingTop: 22
+  },
+  fullNameView: {
+    borderColor: color.palette.lighterGrey,
+    borderWidth: 1,
+    paddingBottom: 18,
+    paddingRight: 20,
+    paddingTop: 22
+  }
+})
