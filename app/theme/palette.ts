e3528554b882e60405d93d3e15719d5fd99b8d4c
Hefetz, Yitzhak (yh9358) <yh9358@intl.att.com>
2020-02-12 19:23:54 +0200

    done

--- app/theme/palette.ts
+++ app/theme/palette.ts
@@ -3,9 +3,12 @@ export const palette = {
   white: "#ffffff",
   offWhite: "#e6e6e6",
   blue: "#273cfb",
+  darkBlue: "#0d25be",
   orangeDarker: "#EB9918",
   lightGrey: "#f4f5fd",
   lighterGrey: "#CDD4DA",
   angry: "#dd3333",
   border: "#dee2e6",
+  popUpBg: 'rgba(0,0,0,0.7)',
+  greyLight: '#f0f1fb'
 }
