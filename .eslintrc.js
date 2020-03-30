4e7d3ad09885f60a5870e8b34a1a1c21f0264424
Hamu, Avraham <avraham.hamu@intl.att.com>
2020-03-30 23:59:26 +0300

    improving eslint infrastruture, and splitting between lint errors display and lint fixing

---
+++ .eslintrc.js
@@ -0,0 +1,70 @@
+module.exports = {
+    "parser": "@typescript-eslint/parser",
+    "extends": [
+      "airbnb-typescript",
+      "eslint:recommended",
+      "prettier", 
+      "prettier/@typescript-eslint", 
+      "prettier/react",
+      "plugin:@typescript-eslint/eslint-recommended",
+      "plugin:@typescript-eslint/recommended",
+      "plugin:react/recommended",
+      "plugin:react-native/all",
+      "standard"
+    ],
+    "parserOptions": {
+      "ecmaFeatures": {
+        "jsx": true
+      },
+      "project": "./tsconfig.json"
+    },
+    "plugins": [
+      "@typescript-eslint",
+      "react",
+      "react-native"
+    ],
+    "settings": {
+      "react": {
+        "pragma": "React",
+        "version": "detect"
+      }
+    },
+    "globals": {
+      "__DEV__": false,
+      "jasmine": false,
+      "beforeAll": false,
+      "afterAll": false,
+      "beforeEach": false,
+      "afterEach": false,
+      "test": false,
+      "expect": false,
+      "describe": false,
+      "jest": false,
+      "it": false
+    },
+    "rules": {
+      "comma-dangle": 0,
+      "no-dupe-class-members": "off",
+      "no-unused-vars": 1,
+      "no-undef": 1,
+	  "quotes": 0,
+	  "semi": ["warn", "always"],
+      "react/no-unescaped-entities": 0,
+      "react/prop-types": "off",
+      "react-native/no-raw-text": 0,
+      "space-before-function-paren": 0,
+      "@typescript-eslint/ban-ts-ignore": 0,
+      "@typescript-eslint/indent": 1,
+      "@typescript-eslint/explicit-member-accessibility": 0,
+      "@typescript-eslint/explicit-function-return-type": 0,
+      "@typescript-eslint/member-delimiter-style": 0,
+      "@typescript-eslint/no-dupe-class-members": [
+        "error"
+	  ],
+      "@typescript-eslint/no-explicit-any": 0,
+      "@typescript-eslint/no-object-literal-type-assertion": 0,
+      "@typescript-eslint/no-empty-interface": 0,
+      "@typescript-eslint/no-var-requires": 0,
+	  "@typescript-eslint/no-use-before-define": 1
+    }
+  }
