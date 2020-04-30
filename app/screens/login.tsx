0a370dd6286330fdba4bd374ba9bbfbc005c6caf
Hamu, Avraham <avraham.hamu@intl.att.com>
2020-04-30 07:53:09 +0300

    PR #17 fixes Loading generic component Error modal generic component

--- app/screens/login.tsx
+++ app/screens/login.tsx
@@ -2,20 +2,24 @@ import React from 'react';
 import { observer } from 'mobx-react-lite';
 import { StyleSheet, View ,TouchableOpacity } from 'react-native';
 import { NavigationActions, NavigationInjectedProps } from 'react-navigation';
-import { Button, Checkbox, Screen, Icon, TextField, Text } from '../components';
 import { color, spacing } from '../theme';
 import { Toggle } from 'react-powerplug';
 import { useStores } from '../models/root-store';
-import { FancyAlert, LoadingIndicator } from 'react-native-expo-fancy-alerts';
+import { FancyAlert } from 'react-native-expo-fancy-alerts';
 import VectorIcon from 'react-native-vector-icons/FontAwesome';
 import PromiseTimeout from '../services/timers/promise-timeout.service';
+import { Button, Checkbox, Screen, Icon, TextField } from '../components';
+import ErrorModal from '../components/modal-error/error.modal';
+import LoadingModal from '../components/loading/loading.modal';
 
 
 export interface LoginProps extends NavigationInjectedProps<{}> {}
 
-export const LoginScreen: React.FunctionComponent<LoginProps> = observer(props => {
+let errorMsg = undefined;
 
-	const [username, setUserName] = React.useState<string>(__DEV__ ? 'dr7774-9-2' : '');
+export const LoginScreen: React.FunctionComponent<LoginProps> = observer(() => {
+
+	const [username, setUserName] = React.useState<string>(__DEV__ ? 'dr7774-9_2' : '');
 	const [password, setPassword] = React.useState<string>(__DEV__ ? '0523057774' : '');
 	const { navigationStore, profileModel: { login } } = useStores();
 
@@ -24,24 +28,28 @@ export const LoginScreen: React.FunctionComponent<LoginProps> = observer(props =
 		setErrorModalState(!isErrorModalDisplayed);
 	}, [isErrorModalDisplayed]);
 
-	const [isSpinnerModalDisplayed, setSpinnerModalState] = React.useState<boolean>(false);
-	const displaySpinnerModal = (value:boolean) => {
-		setSpinnerModalState(value);
+	const [isLoadingModalDisplayed, setLoadingModalState] = React.useState<boolean>(false);
+	// const displayLoadingModal = React.useCallback((newValue) => {
+	// 	setLoadingModalState(newValue);
+	// }, [isLoadingModalDisplayed]);
+	const displayLoadingModal = (show:boolean) => {
+		setLoadingModalState(show);
 	}
 
  	const loginSequence = async () => {
-		displaySpinnerModal(true);
+		displayLoadingModal(true);
 		
 		const loginRequest = login(username, password);
 		const runLoginRequest = PromiseTimeout(10000, loginRequest);
 
 		try {
 			const loginResponse = await runLoginRequest;
-			displaySpinnerModal(false);
+			displayLoadingModal(false);
 			handleLoginRequest(loginResponse);
 		} catch (error) {
 			console.log(`error: ${error}`);
-			displaySpinnerModal(false);
+			displayLoadingModal(false);
+			errorMsg = 'An error occurred when logging, please try again later...';
 			toggleErrorModal();
 		}
 	}
@@ -51,11 +59,10 @@ export const LoginScreen: React.FunctionComponent<LoginProps> = observer(props =
 			console.log('login success');
 			navigationStore.dispatch(NavigationActions.navigate({ routeName: 'packagesTabList' }))
 		} else {
-			let errorMsg = undefined;
 			if(loginResponse.status === 401 ) {
 				errorMsg = 'The username or password is incorrect';
 			} else {
-				errorMsg = 'An error occurred when logging, please try again later';
+				errorMsg = 'An error occurred when logging, please try again later...';
 			}
 			console.log(errorMsg);
 			toggleErrorModal();
@@ -93,29 +100,13 @@ export const LoginScreen: React.FunctionComponent<LoginProps> = observer(props =
 
 	const renderErrorModal = (): React.ReactElement => {
 		return (
-			<View>
-				<FancyAlert visible={isErrorModalDisplayed} 
-							icon = {
-								<View style={styles.loginModalIcon}>
-									<VectorIcon name="close" size={20} color='white' />
-								</View>
-							}
-							style={{ backgroundColor: color.palette.white }}
-							onRequestClose={() => /*toggleErrorModal()*/ console.log('is it in use??')}
-				>
-					<Text style={styles.loginModalMsg}>An error occurred when logging, please try again later...</Text>
-					<TouchableOpacity style={styles.modalButton}
-									onPress={toggleErrorModal}>
-						<Text style={{color : color.palette.white, textAlign: 'center'}}>OK</Text>
-					</TouchableOpacity>
-				</FancyAlert>
-			</View>
+			<ErrorModal visible={isErrorModalDisplayed} message={errorMsg}/>
 		);
 	}
 
-	const renderSpinnerModal  = (): React.ReactElement => {
+	const renderLoadingModal = (): React.ReactElement => {
 		return (
-			<LoadingIndicator visible={isSpinnerModalDisplayed} />
+			<LoadingModal visible={isLoadingModalDisplayed} />
 		);
 	}
 
@@ -125,7 +116,7 @@ export const LoginScreen: React.FunctionComponent<LoginProps> = observer(props =
 				<Icon style={styles.loginLogo} icon='loginLogo' />
 				{renderTextFields()}
 				{renderCheckbox()}
-				{renderSpinnerModal()}
+				{renderLoadingModal()}
 				{renderErrorModal()}
 				<Button text={'כניסה'} onPress={loginSequence}/>
 			</Screen>
@@ -151,28 +142,6 @@ const styles = StyleSheet.create({
 	rememberMeContainer: {
 		flexDirection: 'row-reverse',
 		marginBottom: spacing.bigSpacing,
-		width: '100%'
-	},
-	loginModalIcon: {
-		flex: 1,
-		display: 'flex',
-		justifyContent: 'center',
-		alignItems: 'center',
-		backgroundColor: 'red',
-		borderRadius: 50,
-		width: '100%'
-	},
-	loginModalMsg: {
-		color: color.palette.black,
-		textAlign: 'center',
-		marginTop: -15,
-		marginBottom: 15
-	},
-	modalButton: {
-		backgroundColor: 'red',
-		borderRadius: 50,
-		padding: 10,
-		marginBottom: 8,
 		width: '100%'
 	}
 });
